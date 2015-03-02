3b5e686badb2445ccfc909c007c3b4ff29d709e2
Ted Goldstein <tedgoldstein@gmail.com>
2015-03-02 14:38:55 -0800

    Requires Meteor.startup in the nodejs so that things execute in the right order at startup.

--- server/methods.js
+++ server/methods.js
@@ -11,37 +11,40 @@ HTTP.methods({
 
 
 
-Meteor.methods({
-    ingestOncore : function() {
-        ingestOncore();
-    },
+Meteor.startup(
+  function() {
+    Meteor.methods({
+        ingestOncore : function() {
+            ingestOncore();
+        },
 
-    addCRF : function(insertDoc, updateDoc) {
-       var crf = insertDoc.crf;
-        delete insertDoc["crf"];
-        var coll = CRFcollections[crf];
-        var _id;
+        addCRF : function(insertDoc, updateDoc) {
+           var crf = insertDoc.crf;
+            delete insertDoc["crf"];
+            var coll = CRFcollections[crf];
+            var _id;
 
-        if (crf in ComplexIDFields) {
-            _id = ""
-            _.each(ComplexIDFields[crf], function(f) {
-                if (_id.length > 0)
-                    _id += " " + insertDoc[f];
-                else
-                    _id += insertDoc[f];
-            })
-        } else {
-            _id = insertDoc["Patient_ID"];
+            if (crf in ComplexIDFields) {
+                _id = ""
+                _.each(ComplexIDFields[crf], function(f) {
+                    if (_id.length > 0)
+                        _id += " " + insertDoc[f];
+                    else
+                        _id += insertDoc[f];
+                })
+            } else {
+                _id = insertDoc["Patient_ID"];
+            }
+
+
+            insertDoc._id = _id;
+            console.log(insertDoc);
+            if (coll.findOne({_id: _id}))
+                coll.update({_id: _id}, updateDoc);
+            else
+                coll.insert(insertDoc);
+            //var ret = coll.upsert({_id: _id}, {$set: insertDoc});
+            // console.log("upsert", ret)
         }
-
-
-        insertDoc._id = _id;
-        console.log(insertDoc);
-        if (coll.findOne({_id: _id}))
-            coll.update({_id: _id}, updateDoc);
-        else
-            coll.insert(insertDoc);
-        //var ret = coll.upsert({_id: _id}, {$set: insertDoc});
-        // console.log("upsert", ret)
-    }
+    })
 })

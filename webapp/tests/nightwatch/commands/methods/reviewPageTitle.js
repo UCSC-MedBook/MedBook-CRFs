493870c65bdde491530e8b6012c4cb5d8abe85ba
Abigail Watson <awatson1978@gmail.com>
2015-06-01 20:53:49 -0500

    Revert "Readonly Test Coverage of Medbook-CRFs"

--- webapp/tests/nightwatch/commands/methods/reviewPageTitle.js
+++
@@ -1,7 +0,0 @@
-exports.command = function(pageTitle) {
-  this
-    .verify.elementPresent(".title-wrapper")
-    .verify.containsText(".title-wrapper", pageTitle)
-
-  return this;
-};
--- webapp/tests/nightwatch/commands/selectFromSidebar.js
+++
@@ -1,8 +0,0 @@
-exports.command = function(sidebarButtonElement) {
-
-  this
-    .verify.elementPresent(sidebarButtonElement)
-    .click(sidebarButtonElement).pause(1000)
-
-  return this; // allows the command to be chained.
-};

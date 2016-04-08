f5e56250cee50e1ae5e8c17b9a33485fcc360af2
Teo Fleming <mokolodi1@gmail.com>
2016-04-07 18:13:24 -0700

    looked through publish functions

--- webapp/server/publish.js
+++ webapp/server/publish.js
@@ -1,74 +1,78 @@
-
 Meteor.publish('patient', function(patient_id, study_id) {
-  console.log("publish patient", patient_id);
-  if (this.userId == null)
-      return [];
+  check([patient_id, study_id], [String]);
 
-  var user = Meteor.users.findOne({_id: this.userId});
-  var collaborations = user.getAssociatedCollaborations();
-  return CRFs.find({Study_ID: study_id,
-		  $or: [
-		      {Patient_ID: patient_id},
-		      {Sample_ID: { $regex: "^" + patient_id + ".*"}}
-		  ]});
+  console.log("publish patient", patient_id);
+
+  var user = MedBook.ensureUser(this.userId);
+  var collaborations = user.getCollaborations();
+  var study = Studies.findOne({id: study_id});
+  user.ensureAccess(study);
+
+  return CRFs.find({
+    Study_ID: study_id,
+    $or: [
+      {Patient_ID: patient_id},
+      {Sample_ID: { $regex: "^" + patient_id + ".*"}}
+    ]
+  });
 });
 
 Meteor.publish('myForms', function(formName, studyName) {
+  check([formName, studyName], [String]);
+
   console.log("publish myForms", formName, studyName);
-  if (this.userId == null)
-      return [];
-  var user = Meteor.users.findOne({_id: this.userId});
-  var collaborations = user.getAssociatedCollaborations();
 
-  var study = Collections.studies.findOne({id: studyName, 
-      $or: [ 
-	    { public: true},
-	    { collaborations: {$in: collaborations }}
-	    ]
+  var user = MedBook.ensureUser(this.userId);
+  var collaborations = user.getCollaborations();
+
+  // var study = Collections.studies.findOne({id: studyName,
+  //   $or: [
+  //     { public: true},
+  //     { collaborations: {$in: collaborations }}
+  //   ]
+  // });
+  // if (study == null) {
+  //     console.log("no study");
+  //     return [];
+  // }
+  var study = Studies.findOne({id: studyName});
+  user.ensureAccess(study);
+
+  var coll = Collections.Metadata.findOne({
+    study: { $in: [ "common", studyName]},
+    name: formName
   });
-  if (study == null) {
-      console.log("no study");
-      return [];
+  if (!coll) {
+    throw new Error("Could not find Metadata for form " + formName + " in study " + studyName);
   }
-
-  var coll = Collections.Metadata.findOne({study: { $in: [ "common", studyName]}, name: formName});
-  if (coll == null)
-      throw new Error("Could not find Metadata for form " + formName + " in study " + studyName);
 
   var q = {CRF:formName};
 
   var schema = JSON.parse(coll.schema);
-  if ("study" in schema)
-       q.study = studyName;
-  else if ("studies" in schema)
-       q.study = {$in: studyName};
-  else if ("Study_ID" in schema)
-       q.Study_ID = studyName;
-
-  if (coll) {
-      var cursor =  Collections.CRFs.find(q);
-      console.log("publish myForms", formName, "count=", cursor.count());
-      return cursor
+  if ("study" in schema) {
+    q.study = studyName;
+  } else if ("studies" in schema) {
+    q.study = {$in: studyName};
+  } else if ("Study_ID" in schema) {
+    q.Study_ID = studyName;
   }
-  console.log("myForms empty");
-  return [];
+
+  var cursor =  Collections.CRFs.find(q);
+  console.log("publish myForms", formName, "count=", cursor.count());
+  return cursor
 });
 
 
 Meteor.publish('studies', function() {
-  if (this.userId == null)
-      return [];
-
-  var user = Meteor.users.findOne({_id: this.userId});
-  var collaborations = user.getAssociatedCollaborations();
+  var user = MedBook.ensureUser(this.userId);
+  var collaborations = user.getCollaborations();
 
   var cursor = Collections.studies.find({
-      $or: [ 
-	    { public: true},
-	    { collaborations: {$in: collaborations }}
-	    ]
+    $or: [
+      { public: true},
+      { collaborations: {$in: collaborations }}
+    ]
   });
   console.log("publish studies", cursor.count());
   return cursor;
 });
-
